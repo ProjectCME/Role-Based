a6a8746c521c2bbc42fa892912c8e3953bc5e0ea
Ujjawal-Kantt <uk1287@srmist.edu.in>
2025-11-20 00:33:36 +0530

    Added UserDTO class

---
+++ src/main/java/dto/Userdto.java
@@ -0,0 +1,38 @@
+// This DTO (Data Transfer Object) class is used to transfer user data and it is used to encapsulate user information.
+
+package com.example.roleapp.dto;
+
+public class UserDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String role;
+    private boolean isStatus;
+
+    public UserDto() {}
+
+    public UserDto(Long id, String name, String email, String role, boolean isStatus) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.isStatus = isStatus;
+    }
+
+    // getters and setters
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+
+    public boolean isStatus() { return isStatus; }
+    public void setStatus(boolean status) { this.isStatus = status; }
+}
